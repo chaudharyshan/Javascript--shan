6f3132bd39bc2bddfca22b64d72a9408a5313d38
chaudharyshan <42136097+chaudharyshan@users.noreply.github.com>
2023-09-20 05:13:30 +0000

    Datatype

---
+++ .devcontainer/01_basic/01_variables.js
@@ -0,0 +1,20 @@
+const accountId = 1222345
+let accuontEmail = "hittesh@gmail.com"
+var accountPassword = "12345"
+accountCity="mansa"
+let accountState;
+
+// accountId = 2
+
+accuontEmail = "shan@gmail.com"
+accountPassword = "123425"
+accountCity = "Ahamdabad"
+
+console.log(accountId);
+
+/*
+Perfer not to use car
+Because of issue of block and function node 
+*/
+
+console.table([accountId,accountPassword,accountCity,accuontEmail,accountState])
---
+++ .devcontainer/01_basic/02_dataType.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all Js code as newer version 
+
+// alert(3+3) // we are useing node js
+
+console.log(3+3);
+
+console.log("shan")
+
+let name = "shan"
+let age = 20
+let isLoggedIt = false 
+
+// number => 2 to power 53
+// bigint
+// String => ""
+// booleas => true/false
+//null => standalone value - object
+// undefined => 
+// symbol => unique
+
+// object
+
+console.log(typeof undefined); // type
+console.log(typeof null); // object
