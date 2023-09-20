67a45664e1d79e7bf8bfbb59d8c5709c45b6aec4
chaudharyshan <42136097+chaudharyshan@users.noreply.github.com>
2023-09-20 16:15:03 +0000

    data type summary

---
+++ .devcontainer/01_basic/datatype_summary.js
@@ -0,0 +1,34 @@
+// Primitve 
+
+// 7 types : String, Number, Boolearn, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue =100.3
+
+const isLoggedIn = false
+const putsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+
+const bigNumber = 23455389026405820576038452072605498967402582962099n
+
+
+
+//-------------------------
+
+
+// Reference (Non primitive)
+
+// Array, Objects, Functions - typeof objects
+
+const heros = ["shaktiman", "naagraj", "doga"]
+let myobj ={
+    name: " shan",
+    age:24
+}
+
+const myfunction = function () {
+    console.log("hello")
+    
+}    
