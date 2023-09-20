d64556ab693657a8078bec24641a115d12990fd5
chaudharyshan <42136097+chaudharyshan@users.noreply.github.com>
2023-09-20 06:54:45 +0000

    conversion of datatype

---
+++ .devcontainer/01_basic/03_conversionOperation.js
@@ -0,0 +1,31 @@
+let score = null
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+//"33" => 33
+//"33abc" => NaN
+
+
+let isLoggedIn = 0
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// true => 1; false =>0
+
+// "" => false  
+
+// "shan" => true
+
+let someNumber = 33 
+
+let stringNumber = String(someNumber)
+
+
+console.log(stringNumber);
+console.log(typeof stringNumber);
