313abaa5157c8a591fe83a89ff272ecfd6c0662f
chaudharyshan <42136097+chaudharyshan@users.noreply.github.com>
2023-09-20 15:41:15 +0000

    conversion operation

--- .devcontainer/01_basic/03_conversionOperation.js
+++ .devcontainer/01_basic/03_conversionOperation.js
@@ -1,11 +1,11 @@
 let score = null
 
-console.log(typeof score);
-console.log(typeof (score));
+// console.log(typeof score);
+// console.log(typeof (score));
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 //"33" => 33
 //"33abc" => NaN
@@ -14,7 +14,7 @@ console.log(valueInNumber);
 let isLoggedIn = 0
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 // true => 1; false =>0
 
@@ -27,5 +27,38 @@ let someNumber = 33
 let stringNumber = String(someNumber)
 
 
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+// ************** Operations ****************
+
+let value = 3
+let negValue = -3
+
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+//  console.log(2%3);
+
+let str1 = "Hello"
+let str2 = " shan"
+
+let str = str1+str2
+
+
+let num1, num2, num3
+
+let gameCounter = 100  
+gameCounter++;
+console.log(gameCounter);
+
+let gameCounterPrefix = 100  
+++gameCounterPrefix;
+console.log(gameCounterPrefix);
+
+
