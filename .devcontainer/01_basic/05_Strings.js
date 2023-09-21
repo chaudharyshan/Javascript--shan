f14d71745b4de2fc890ca0b12724262691d17228
chaudharyshan <chaudharyshan1111@gmail.com>
2023-09-21 10:41:43 +0530

    strings

---
+++ .devcontainer/01_basic/05_Strings.js
@@ -0,0 +1,13 @@
+const name = "shan"
+const repoCount = "50"
+
+// console.log(name + repoCount + "value");
+
+console.log(`Hello my name is ${name} my repo count is ${repoCount}`); 
+
+const gameName = new String('shan') // string object 
+
+console.log(gameName[0]);
+console.log(gameName.toUpperCase());
+
+
