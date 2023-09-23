052ffceaab7a3e8c706fde9423042077cd4fa031
chaudharyshan <chaudharyshan1111@gmail.com>
2023-09-23 17:14:23 +0530

    Array

---
+++ .devcontainer/02_basc/01_arrays.js
@@ -0,0 +1,43 @@
+//Array
+
+const myArr = [0, 1, 2, 3, 4, 5]
+
+const myHero = ["shan", "iron Man"]
+
+ 
+const myArr2 = new Array(1, 2, 3, 4, 5)
+// console.log(myArr[0]);
+
+
+// Arry methods
+
+
+// myArr.push(4)
+// myArr.pop(3)
+
+
+// myArr.unshift(9)
+// myArr.shift()
+
+// console.log(myArr.includes(9));
+// console.log(myArr.indexOf(4));
+
+// const newArr = myArr.join()
+
+// console.log(myArr);
+// console.log(newArr);
+
+
+// slice, splice
+
+console.log("A", myArr);
+
+const myn1 = myArr.slice(1, 3)
+
+console.log(myn1);
+console.log("B", myArr);
+
+const myn2 = myArr.splice(1, 3)
+
+console.log(myn2);
+console.log("C", myArr);
