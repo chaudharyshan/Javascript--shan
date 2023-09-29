2c36fb50dd5694c5ab5b0b537126733cf49f5c20
chaudharyshan <chaudharyshan1111@gmail.com>
2023-09-29 23:06:05 +0530

    function_final

--- .devcontainer/03_basic/01_functions.js
+++ .devcontainer/03_basic/01_functions.js
@@ -41,4 +41,43 @@ function loginUserMessage(username = "sam") {
 
 
 
-console.log(loginUserMessage());
+// console.log(loginUserMessage());
+
+function calculateCartPrice(val1, val2, ...num1) {
+    return num1
+
+    
+}
+
+// console.log(calculateCartPrice(20, 400, 500));
+
+const user = {
+    username: " shan",
+    price: 200
+}
+
+function handleObject(anyobject) {
+
+    console.log(`username is ${anyobject.username} and price is ${anyobject.price}`);
+    
+}
+
+// handleObject(user)
+
+handleObject({
+    username : "sam",
+    price : 399
+})
+
+
+const myArray = [200, 300, 500, 600]
+
+function returnSecondValue(getArray){
+    return getArray [1]
+    
+}
+
+// console.log(returnSecondValue(myArray));
+console.log(returnSecondValue(
+    [200,500,600,700]
+));
