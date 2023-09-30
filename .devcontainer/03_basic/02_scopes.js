d61ee6cacd5410bdf99d2094d07871dc94869c34
chaudharyshan <chaudharyshan1111@gmail.com>
2023-09-30 11:32:37 +0530

    scops01

--- .devcontainer/03_basic/02_scopes.js
+++ .devcontainer/03_basic/02_scopes.js
@@ -1,1 +1,24 @@
-let a =10
+let a =300
+
+if (true) {
+    let a = 30
+    const b = 20
+    // console.log("under: ", a);
+}
+
+// console.log("bar: ", a);
+
+function one() {
+    
+    let username = "shan"
+
+    function two() {
+      
+        const website = "youtube"
+        console.log("Name ", username);
+        }
+        two()
+        console.log(username);
+}
+
+one()
