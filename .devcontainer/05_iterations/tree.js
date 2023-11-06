74e126c4647ccdff5aab17c29c3d0e8300bde5f7
chaudharyshan <chaudharyshan1111@gmail.com>
2023-11-06 18:59:52 +0530

    loops

---
+++ .devcontainer/05_iterations/tree.js
@@ -0,0 +1,22 @@
+// for of
+
+const arr = [ 1, 2, 3, 4, 5+1] 
+
+for (const num of arr) {
+    // console.log(num);
+}
+
+//----  ---//
+
+const greetings = "hello world"
+
+for (const greet of greetings) {
+    // console.log(`Each char ${greet}`);
+    
+}
+
+//---- Map ---//
+
+const map = new Map ()
+
+
---
+++ .devcontainer/05_iterations/two.js
@@ -0,0 +1,27 @@
+
+let i = 0
+while (i<=100) {
+    // console.log(`value of i ${i}`);
+    i = i +2
+}
+
+
+let myArray = ['shan', 'batman', ' superman']
+
+let arr = 0
+
+while (arr < myArray.length) {
+
+    // console.log(`value is ${myArray[arr]}`);
+    arr = arr + 1
+    
+}
+
+let score = 0
+
+do {
+
+    console.log(`score is ${score}`);
+    score ++
+    
+} while (score<= 10);
