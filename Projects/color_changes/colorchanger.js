92ebd14ace3ce4e606e947afb59ed745c24d3f3f
chaudharyshan <chaudharyshan1111@gmail.com>
2023-11-07 16:51:20 +0530

    project

---
+++ Projects/color_changes/colorchanger.js
@@ -0,0 +1,11 @@
+const buttons = document.querySelectorAll('.button')
+const body = document.querySelector("body")
+
+// console.log(buttons);
+
+buttons.forEach( function (button) {
+    console.log(button);
+    button.addEventListener('click', function () {
+        
+    })
+})
